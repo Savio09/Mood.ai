89c8f8370d1be1e36ce89020f82d421c74d2a6c6
Savio09 <declanfortune5@gmail.com>
2024-11-14 12:39:14 -0800

    added frontend and apis

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,40 @@
+import EntryCard from "@/components/EntryCard";
+import NewEntryCard from "@/components/NewEntryCard";
+import { getUserByClerkID } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+import Link from "next/link";
+
+const getEntries = async () => {
+  const user = await getUserByClerkID();
+  const entries = prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      analysis: true,
+    },
+  });
+  return entries;
+};
+const JournalPage = async () => {
+  const entries = await getEntries();
+
+  return (
+    <div className="p-10 bg-zinc-400/10 h-full">
+      <h2 className="text-3xl mb-8">Journal </h2>
+      <div className="grid grid-cols-3 gap-4">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <Link href={`/journal/${entry.id}`}>
+            <EntryCard key={entry.id} entry={entry} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default JournalPage;
---
+++ app/new-user/page.tsx
@@ -0,0 +1,30 @@
+import { prisma } from "@/utils/db";
+import { ClerkLoading } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const createNewUSer = async () => {
+  const user = await currentUser();
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+  redirect("/journal");
+};
+const NewUser = async () => {
+  await createNewUSer();
+  return <ClerkLoading />;
+};
+
+export default NewUser;
